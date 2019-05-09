c4541067d5619741c13aa3d3ed55b1295b2f19ac
zhangjian <279694714@qq.com>
2019-05-09 16:03:31 +0800

    sf

--- myboot1/src/main/java/com/zb/Myboot1Application.java
+++ myboot1/src/main/java/com/zb/Myboot1Application.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @PropertySource("classpath:/config/config.properties")
 public class Myboot1Application {
+    //测试
     public static void main(String[] args) {
         SpringApplication.run(Myboot1Application.class, args);
     }
